c2e1ffc22c399498511c49f5ee249426334b7e9e
NAKAJIMA Yusaku <yupong7@gmail.com>
2023-04-20 07:01:58 +0900

    typo

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -8,10 +8,7 @@ import { HEROES } from '../mock-heroes';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent {
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm'
-  };
+
   heroes = HEROES;
   selectedHero?: Hero;
   
